fd0b640e5f4bdcf6d95ddfedb354724b355170da
Claudia413 <cmjengelsman@gmail.com>
2017-05-17 15:09:54 +0200

    Added more def messages for customer

--- yogastore.rb
+++ yogastore.rb
@@ -11,10 +11,17 @@
   { reference_number: 1238, name: "Light On Yoga", price: 10 }
 ]
 
+#Messages for customer
+
 def welcome_message
   puts "Hello, how can I help you today?"
 end
 
 def items_on_sale
   puts "These are our items for sale today"
+end
+
+def shop_or_checkout
+  puts "Thank you, would you like to continue shopping(1) or proceed to check out(2)?('1' or '2')"
+  shop_answer = gets.chomp
 end
